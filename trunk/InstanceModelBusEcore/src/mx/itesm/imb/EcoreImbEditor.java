1f91946823bc055f4927e8e86a14ff9b52d87397
jccastrejon <jccastrejon@1f071e9a-5a95-a6ef-bdb8-f41edce5e247>
2010-11-08 20:55:22 +0000

    Initial import.

---
+++ trunk/InstanceModelBusEcore/src/mx/itesm/imb/EcoreImbEditor.java
@@ -0,0 +1,182 @@
+package mx.itesm.imb;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
+/**
+ * 
+ * @author jccastrejon
+ * 
+ */
+public class EcoreImbEditor {
+
+    /**
+     * 
+     */
+    private static Template aspectTemplate;
+
+    static {
+        EcoreImbEditor.aspectTemplate = EcoreImbEditor.getEcoreTemplate();
+    }
+
+    public static void main(String[] args) {
+        // 0: ecoreProject, 1: busProject, 2: templateProject
+        EcoreImbEditor.generateProvidersAspects(new File(args[0]));
+        EcoreImbEditor.configureRestTemplate(new File(args[0]), new File(args[1]), new File(args[2]));
+    }
+
+    /**
+     * 
+     * @param ecoreProject
+     */
+    @SuppressWarnings("unchecked")
+    public static void generateProvidersAspects(final File ecoreProject) {
+        File provider;
+        Iterator<File> providers;
+
+        // Get providers files
+        providers = FileUtils.iterateFiles(new File(ecoreProject.getParent(), ecoreProject.getName() + ".edit"),
+                new IOFileFilter() {
+
+                    @Override
+                    public boolean accept(final File file) {
+                        return file.getName().endsWith("ItemProvider.java");
+                    }
+
+                    @Override
+                    public boolean accept(File directory, String file) {
+                        return file.endsWith("ItemProvider.java");
+                    }
+                }, TrueFileFilter.INSTANCE);
+
+        while (providers.hasNext()) {
+            try {
+                provider = providers.next();
+                EcoreImbEditor.writeEcoreAspect(provider);
+                System.out.println("Aspect for " + provider + " successfully generated");
+            } catch (Exception e) {
+                System.out.println("Error generating aspect: " + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param ecoreProject
+     * @param busProject
+     * @param templateProject
+     */
+    @SuppressWarnings("unchecked")
+    public static void configureRestTemplate(final File ecoreProject, final File busProject, final File templateProject) {
+        File manifestFile;
+        String configuration;
+        String exportPackage;
+        String bundleActivator;
+        String templateContent;
+        String oxmConfiguration;
+
+        try {
+            // Copy lib
+            FileUtils.copyDirectory(new File(templateProject, "/lib.rest"), new File(ecoreProject.getParent(),
+                    ecoreProject.getName() + ".edit/lib"));
+
+            // Add rest configuration
+            templateContent = FileUtils.readFileToString(new File(templateProject, "/templates/beans.xml"));
+            configuration = FileUtils.readFileToString(new File(busProject,
+                    "/src/main/resources/META-INF/spring/applicationContext-contentresolver.xml"));
+            oxmConfiguration = configuration.substring(configuration.indexOf("<oxm:"),
+                    configuration.indexOf("</oxm:jaxb2-marshaller>") + "</oxm:jaxb2-marshaller>".length());
+            templateContent = templateContent.replace("<!-- oxm -->", oxmConfiguration);
+            FileUtils.writeStringToFile(new File(ecoreProject.getParent(), ecoreProject.getName() + ".edit/beans.xml"),
+                    templateContent);
+
+            // Copy imb types
+            FileUtils.copyDirectory(new File(busProject, "/src/main/java/imb"), new File(ecoreProject.getParent(),
+                    ecoreProject.getName() + ".edit/src/imb"));
+
+            // Update classpath
+            FileUtils.copyFile(new File(templateProject, "templates/classpath.xml"), new File(ecoreProject.getParent(),
+                    ecoreProject.getName() + ".edit/.classpath"));
+            templateContent = FileUtils.readFileToString(new File(templateProject, "/templates/project.xml"));
+            FileUtils.writeStringToFile(new File(ecoreProject.getParent(), ecoreProject.getName() + ".edit/.project"),
+                    templateContent.replace("${projectName}", ecoreProject.getName()));
+
+            // Update Manifest
+            bundleActivator = null;
+            exportPackage = null;
+            manifestFile = new File(ecoreProject.getParent(), ecoreProject.getName() + ".edit/META-INF/MANIFEST.MF");
+            for (String line : (List<String>) FileUtils.readLines(manifestFile)) {
+                if (line.startsWith("Bundle-Activator")) {
+                    bundleActivator = line.replace("Bundle-Activator: ", "");
+                } else if (line.startsWith("Export-Package: ")) {
+                    exportPackage = line.replace("Export-Package: ", "");
+                }
+            }
+            templateContent = FileUtils.readFileToString(new File(templateProject, "/templates/manifest.MF"));
+            templateContent = templateContent.replaceAll("\\$\\{projectName\\}", ecoreProject.getName());
+            templateContent = templateContent.replace("${bundleActivator}", bundleActivator);
+            templateContent = templateContent.replace("${exportPackage}", exportPackage);
+            FileUtils.writeStringToFile(manifestFile, templateContent);
+        } catch (Exception e) {
+            System.out.println("Error while configuring Rest template: " + e.getMessage());
+        }
+    }
+
+    /**
+     * 
+     * @param typeName
+     * @throws IOException
+     */
+    private static void writeEcoreAspect(final File provider) throws IOException {
+        Writer writer;
+        String typeName;
+        String packageName;
+        VelocityContext context;
+
+        typeName = provider.getName().replace(".java", "");
+        packageName = provider.getPath()
+                .substring(provider.getPath().indexOf("src") + 4, provider.getPath().indexOf(typeName) - 1)
+                .replace('/', '.');
+
+        context = new VelocityContext();
+        context.put("typeName", typeName);
+        context.put("packageName", packageName);
+
+        writer = new FileWriter(new File(provider.getParentFile(), "EcoreAspect_" + typeName + ".aj"));
+        EcoreImbEditor.aspectTemplate.merge(context, writer);
+        writer.close();
+    }
+
+    /**
+     * 
+     */
+    private static Template getEcoreTemplate() {
+        Template returnValue;
+        Properties velocityProperties;
+        try {
+            velocityProperties = new Properties();
+            velocityProperties.put("resource.loader", "class");
+            velocityProperties.put("class.resource.loader.class",
+                    "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            Velocity.init(velocityProperties);
+            returnValue = Velocity.getTemplate("mx/itesm/imb/EcoreAspect.vml");
+        } catch (Exception e) {
+            returnValue = null;
+            System.out.println("Error configuring velocity: " + e.getMessage());
+        }
+
+        return returnValue;
+    }
+}
