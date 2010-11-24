2ce16f3f73cfb51dbcba11338fd8c786217b87f7
jccastrejon <jccastrejon@1f071e9a-5a95-a6ef-bdb8-f41edce5e247>
2010-11-24 00:40:11 +0000

    Initial import for Test Project (TodoList)

---
+++ TodoList.ecore.tests/src/todolist/tests/SystemTest.java
@@ -0,0 +1,92 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package todolist.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import todolist.TodolistFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>System</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class SystemTest extends TestCase {
+
+    /**
+     * The fixture for this System test case.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected todolist.System fixture = null;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public static void main(String[] args) {
+        TestRunner.run(SystemTest.class);
+    }
+
+    /**
+     * Constructs a new System test case with the given name.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public SystemTest(String name) {
+        super(name);
+    }
+
+    /**
+     * Sets the fixture for this System test case.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected void setFixture(todolist.System fixture) {
+        this.fixture = fixture;
+    }
+
+    /**
+     * Returns the fixture for this System test case.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected todolist.System getFixture() {
+        return fixture;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see junit.framework.TestCase#setUp()
+     * @generated
+     */
+    @Override
+    protected void setUp() throws Exception {
+        setFixture(TodolistFactory.eINSTANCE.createSystem());
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see junit.framework.TestCase#tearDown()
+     * @generated
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        setFixture(null);
+    }
+
+} //SystemTest
